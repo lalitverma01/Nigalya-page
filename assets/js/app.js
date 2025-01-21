1f64305b8c40331e4f1834f70c7514053ff54aee
lalitverma01 <lalitverma2923@gmail.comm>
2025-01-21 16:26:45 +0530

    all mistake solve

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,7 @@
-// const navbarbtn = () => {
-//     document.getElementById("name").classList.toggle("show-navbar")
-//     document.getElementById("rotates").classList.toggle("rotate40")
-//     document.getElementById("transparent").classList.toggle("bg-transparent")
-//     document.getElementById("rotates2").classList.toggle("rotate-45")
-//     document.body.classList.toggle("overflow-hidden")
-// }
-
-
-function navbarbtn() {
-    const navLinks = document.getElementById('name');
-    navLinks.classList.toggle('show');
-
-    const rotates = document.getElementById('rotates');
-    const rotates2 = document.getElementById('rotates2');
-
-    rotates.classList.toggle('rotate1');
-    rotates2.classList.toggle('rotate2');
+const navbarbtn = () => {
+    document.getElementById("nav-name").classList.toggle("show-nvabar")
+    document.getElementById("rotats").classList.toggle("rotate40")
+    document.getElementById("transparent").classList.toggle("bg-transparent")
+    document.getElementById("rotats2").classList.toggle("rotate-45")
+    document.body.classList.toggle("overflow-hidden")
 }
